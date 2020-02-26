cdf74731a4ca1213a39b7499db041f9343faf7d2
David Mason <masond@ufl.edu>
2020-02-26 10:14:02 -0500

    saving

---
+++ 25_Feb_Class.R
@@ -0,0 +1,27 @@
+# February 25th 2020—multinomial distribution practice
+
+rm(mult)
+factorial(12)
+factorial(7)*factorial(2)*factorial(3)
+479001600/60480
+7920*(0.40^7)*(0.35^2)*(0.25^3)
+
+
+n = 10
+n1 = 0
+n2 = 3
+n3 = 2
+n4 = 5
+p1 = 0.09
+p2 = 0.21
+p3 = 0.21
+p4 = 0.49
+
+factorial(n)
+factorial(n1)*factorial(n2)*factorial(n3)*factorial(n4)
+3628800/1440
+2520*(p1^n1)*(p2^n2)*(p3^n3)*(p4^n4)
+
+# we are interested in getting probability of not seeing any (pi) and estimation
+# of entire population (n)
+
