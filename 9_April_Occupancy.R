15d873a251d93b62c84b3100bb9381b81603d207
David Mason <masond@ufl.edu>
2020-04-10 13:39:53 -0400

    finished part 1 of instructions

--- 9_April_Occupancy.R
+++ 9_April_Occupancy.R
@@ -1,5 +1,7 @@
 # 9 April 2020—Lab 13 "Occupancy ####
 library(unmarked)
+library(MuMIn)
+# Notes from class discussion ####
 # keep covariates constant
 # don't add parameters to probability of detection, just occupancy, 
 # no additive or interactive models (replace)—keep it like practice
@@ -13,7 +15,14 @@ library(unmarked)
 # question 1 keep winter t as probability for detection, 
 # use other covariates for occupancy
 # wenyi wasn't telling R what the variable is (type = State)
+# new data needs to be a dataframe
+# scale and center distance to main road
+# if we want to determine odds ratio of estimate, we have to back transform
+# the null model (for odds ratio take exponent of variable you are intersted in, if you
+# want the probability you do the backtransform or prediction)
+
 predict(pTemp.psiAgric, newdata = agricultural, type = "state", appendData = TRUE)
+pred=predict(model,type="state",newdata=nwdat,appendData=TRUE))
 
 # Single season occupancy ####
 badger <- read.csv("badger_occupancy_scotland.csv", header = TRUE)
@@ -45,3 +54,12 @@ sd(scale(badger.o@siteCovs$DistMR))
 
 pTemp.psiDTMR <- occu(~WTemp ~scale(DistMR), data = badger.o)
 summary(pTemp.psiDTMR)
+
+models <- fitList( 'pdot.psidot'=pdot.psidot, 
+								 'pTemp.psiTemp'=pTemp.psiTemp, 
+								 'pTemp.psiDTMR'=pTemp.psiDTMR )
+modSel(models)
+out.put <- model.sel(pTemp.psiTemp, pTemp.psiDTMR)
+MA.ests <- model.avg(out.put, subset = delta < 2)
+# Assignment 1 ####
+# Multi-season occupancy ####
